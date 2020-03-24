07d25ae76b9611be7819af75425695b47e3ea287
Hyan18 <HarrisonYan18@gmail.com>
2020-03-24 16:07:00 +0000

    feat: picture component gets picture using API

--- src/components/Picture/Picture.jsx
+++ src/components/Picture/Picture.jsx
@@ -1,17 +1,43 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Picture extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      isLoaded: false,
+      cat: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.thecatapi.com/v1/images/search', { headers: { 'x-api-key': process.env.REACT_APP_CAT_API_KEY } })
+      .then(
+        (response) => {
+          this.setState({
+            isLoaded: true,
+            cat: response.data[0]
+          })
+        })
   }
   
   render() {
+    const { isLoaded, cat } = this.state
 
-    return (
-      <div className="picture-div">
-        <Picture />
-      </div>
-    )
+    if (!isLoaded) {
+      return (
+        <div className="picture-div">
+          <span>Loading...</span>
+        </div>
+      )
+    } else {
+      return (
+        <div className="picture-div">
+          <img alt={cat.id} src={cat.url}/>
+        </div>
+      )
+    }
   }
 }
 
--- src/components/Picture/Picture.spec.js
+++ src/components/Picture/Picture.spec.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Picture from './Picture'
+import axios from 'axios'
+
+jest.mock('axios');
+axios.get.mockImplementation(() => {
+  return Promise.resolve({
+    data: [ { id: "sphy", name: "Sphynx" } ]
+  })
+})
 
 describe('Picture', () => {
   let wrapper
@@ -9,10 +17,18 @@ describe('Picture', () => {
     wrapper = shallow(<Picture />)
   })
 
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   describe('.render', () => {
     it('should render a <div />', () => {
       expect(wrapper.find('.picture-div')).toBeDefined()
       expect(wrapper.find('.picture-div').length).toEqual(1)
     })
+  })
+
+  it('should request a random image from TheCatApi', () => {
+    expect(axios.get.mock.calls[0][0]).toEqual('https://api.thecatapi.com/v1/images/search')
   })
 })
